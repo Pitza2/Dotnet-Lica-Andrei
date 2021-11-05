27df0f517a0ebe1477554a124fad306628d96638
pitza2 <andrei.lica2002@gmail.com>
2021-11-05 13:42:26 +0200

    mesaj

---
+++ Lica-Andrei/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lica_Andrei
+{
+    class Program  // o clasa care are membrii si metode
+    {
+        static void Main(string[] args) //metoda main care se va rula prima la executare
+        {
+            Console.WriteLine("Hello World!"); //o instructiune / statement
+            Program obiect = new Program();
+            string[] argProvizoriu = { "1" };
+            if (args.Length == 0)// in tutorial a bagat argumente la main prin consola. nu stiu cum fac asta in windows asa ca am reapelat main cu inca un argument
+            {
+                Main(argProvizoriu);
+            }
+               
+            obiect.Print();
+            
+        }
+        void Print()
+        {
+            Console.WriteLine("am afisat alt mesaj in afara de hello world yey");
+        }
+    }
+}
